312619a04b1e1d4111181f69c41fda4f8314657f
Morten Olsen <40779401+Min222@users.noreply.github.com>
2019-12-11 13:17:42 +0100

    Module

--- GolfProject.js
+++ GolfProject.js
@@ -1,3 +1,10 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./Firebase/golfproject-db222-firebase-adminsdk-un2g2-be7a0784d4");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://golfproject-db222.firebaseio.com"
+});
+
 class GolfScore {
     constructor(time, color) {
         this.time = time;
@@ -5,19 +12,9 @@ class GolfScore {
     }
 }
 
-function saveData() {
-    let admin = require("firebase-admin");
-    let serviceAccount = require("./Firebase/golfproject-db222-firebase-adminsdk-un2g2-be7a0784d4");
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://golfproject-db222.firebaseio.com"
-    });
-    let db = admin.database();
-    let ref = db.ref("golf");
-
-    let golfScore = new GolfScore(new Date().getTime(), "yellow");
-
-    ref.push(golfScore);
-}
-
-saveData();
+exports.saveScore = function (ballColorString, courseNumber) {
+    let database = admin.database();
+    let reference = database.ref("golf/" + courseNumber);
+    let golfScore = new GolfScore(new Date().getTime(), ballColorString);
+    reference.push(golfScore, a => a);
+};
